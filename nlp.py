b75f3afde068214c5d48be2bfec6caf5bd0117f6
Amit Nijsure <amitnijsure1234@gmail.com>
2017-09-14 18:24:50 +0530

    Added nlp.py using Digital Ocean NLTK first tutorial

---
+++ nlp.py
@@ -0,0 +1,31 @@
+# Import data and tagger
+from nltk.corpus import twitter_samples
+from nltk.tag import pos_tag_sents
+
+
+# Load tokenized tweets
+tweets_tokens = twitter_samples.tokenized('positive_tweets.json')
+
+
+# Tag tokenized tweets
+tweets_tagged = pos_tag_sents(tweets_tokens)
+
+
+# Set counts for POS tags
+JJ_count = 0
+NN_count = 0
+
+
+# Loop through the list of tweets
+for tweet in tweets_tagged:
+    for pair in tweet:
+        tag = pair[1]
+        if tag == 'JJ':
+            JJ_count += 1
+        elif tag == 'NN':
+            NN_count += 1
+
+
+# Print total numbers for each adjectives and nouns
+print("Total number of adjectives = ", JJ_count)
+print("Total number of nouns = ", NN_count)
